a94cd76af66240faeaac349bf094ef098abab466
Yuvrajsanap <125144651+Yuvrajsanap@users.noreply.github.com>
2023-12-30 19:16:53 +0530

    Add files via upload

---
+++ assignment-22/qustion2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+class Pattern{
+    private:
+    int iRow;
+    int iCol;
+
+    public:
+    Pattern(int x,int y){
+        iRow=x;
+        iCol=y;
+    }
+
+    void PatternPrint(){
+        int i;
+        int j;
+        for(i=1;i<=iRow;i++){
+            for(j=1;j<=iCol;j++){
+                if(i==j || i<j ){
+                    cout<<"*\t";
+                }
+            }
+            cout<<"\n";
+        }
+    }
+};
+
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    cout<<"Enter number of rows and column :\n";
+    cin>>iValue1>>iValue2;
+
+    Pattern *obj=new Pattern(iValue1,iValue2);
+
+    obj->PatternPrint();
+
+    return 0;
+}
---
+++ assignment-22/qustion4.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Pattern{
+    private:
+    int iRow=0;
+    int iCol=0;
+
+    public:
+    Pattern(int x,int y){
+        iRow=x;
+        iCol=y;
+    }
+    void PatternPrint(){
+        int i=0;
+        int j=0;
+        for(i=1;i<=iRow;i++){
+            for(j=1;j<=iCol;j++){
+                if(i==j || i==1 || i==iRow ||j==1 || j==iCol){
+                    cout<<"*\t";
+                }
+                else{
+                    cout<<" \t";
+                }
+            }
+            cout<<"\n";
+        }
+    }
+};
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+    cout<<"Enter number of rows and column:\n";
+    cin>>iValue1>>iValue2;
+
+    Pattern *obj=new Pattern(iValue1,iValue2);
+
+    obj->PatternPrint();
+
+    return 0;
+}
